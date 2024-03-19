e1ed2bedccd6e3ec87b68b2070f17ee74c93a72b
lilabeck <lila.beck@maine.edu>
2024-03-19 12:29:40 -0400

    Update KI_standi.R

--- KI_standi.R
+++ KI_standi.R
@@ -48,3 +48,23 @@ CHRONO.crn <- cbind(rownames(CHRONO.crn), data.frame(CHRONO.crn, row.names=NULL)
 CHRONO.crn <- rename.vars(CHRONO.crn, c("rownames(CHRONO.crn)"), c("YEAR"))
 write.table(format(CHRONO.crn, digits=2), file="KImeanscor.TXT", append = FALSE, quote = FALSE, sep = ",", row.names = FALSE, col.names = TRUE)
 
+##KI Balsam fir##
+
+KIbf<-read.tucson("KI_ABBA.raw", header = NULL, long = FALSE, encoding = getOption("encoding"), edge.zeros = TRUE)
+#KIbf<-write.csv(KIbf, "KIbf_fix.csv")
+KIbf_fix<-read.csv("~/Desktop/KIbf_fix.csv")
+KIbf_fix<-KIbf_fix[,2:38]
+
+ids<-read.ids(KIbf_fix, stc=c(2, 4, 1))
+KImeansbf <- treeMean(KIbf, ids, na.rm=TRUE)
+
+chrono_bf=i.detrend(KImeansbf, nyrs = NULL, f=0.5, pos.slope = FALSE)
+
+CHRONO.bf <- chron(chrono_bf, prefix = "Plymouth")
+plot(CHRONO.bf, add.spline=TRUE, nyrs=20, xlim=c(1900, 2025), ylim=c(0.5, 1.5))
+
+CHRONO.bf <- cbind(rownames(CHRONO.bf), data.frame(CHRONO.bf, row.names=NULL))
+CHRONO.bf <- rename.vars(CHRONO.bf, c("rownames(CHRONO.bf)"), c("YEAR"))
+write.table(format(CHRONO.crn, digits=2), file="KIbfcor.TXT", append = FALSE, quote = FALSE, sep = ",", row.names = FALSE, col.names = TRUE)
+
+
